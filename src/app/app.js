274397ae852b05e4f7f474320ce1d664af885cb7
Daniel Osineye <daniel.osineye@gmail.com>
2017-11-17 13:00:08 +0100

    Added react , react-dom | Get simple demo working!

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,28 @@
-console.log("App works!")
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom';
+
+class Entry extends Component{
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      name : "Daniel Osineye"
+    }
+  }
+  handleChange(event){
+    this.setState({name: event.target.value})
+  }
+
+
+  render(){
+    return(
+      <div style={{textAlign:'center'}}>
+        <h1>Welcome</h1>
+        <p>Hello {this.state.name}</p>
+        <input onChange={this.handleChange} type='text' defaultValue={this.state.name} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Entry/>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module:{
     rules:[
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         use:['babel-loader'],
         exclude: /node_modules/
       },
